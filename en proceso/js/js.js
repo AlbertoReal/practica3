eb2b8e0552ece8b35267fa77d9e008bf058f1b1e
AlbertoReal <32370393+AlbertoReal@users.noreply.github.com>
2018-11-28 18:28:43 +0100

    28/11/2018

--- en proceso/js/js.js
+++ en proceso/js/js.js
@@ -8,19 +8,13 @@ function openMenu() {
   document.getElementById("menu_movil").style.display = "block";
   document.getElementById("boton1").style.display = "none";
   document.getElementById("boton2").style.display = "inline-block";
-  document.getElementById("boton2").style.float = "right";
-  document.getElementById("boton2").style.marginRight = "10%";
-  document.getElementById("boton2").style.marginTop = "2.5%";
+
 }
 function closeMenu() {
   document.getElementById("menu_movil").style.display = "none";
   document.getElementById("boton1").style.display = "inline-block";
-  document.getElementById("boton1").style.float = "right";
   document.getElementById("boton2").style.display = "none";
-  document.getElementById("boton1").style.marginRight = "10%";
-  document.getElementById("boton1").style.marginTop = "2.5%";
-  document.getElementById("Practices").style.display = "none";
-  document.getElementById("our_lawyers").style.display = "none";
+
 }
 function openPracticies() {
   document.getElementById("Practices").style.display = "block";
